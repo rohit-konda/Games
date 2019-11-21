fixed pure nash computation

Games/basic.py
@@ -23,11 +23,6 @@ def flatten(iterable):
     return [i for sublist in iterable for i in sublist]
 
 
-def to_dictstrat(players, strategies):
-    """ return a dictionary of strategies for each player from a list of strategies """
-    return {k: v for (k, v) in zip(players, strategies)}
-
-
 def dict_nlist(dic):
     """ create a nested list from a dictionary of indices and values """
 
@@ -57,7 +52,7 @@ class Game:
     def __init__(self, payoffs, players, strategies):
         self.players = players  # list of player labels (use the same ordering for indexing as in this list)
         self.n = len(players)  # number of players
-        self.strategies = strategies  # dictionary of (player: list of possible strategies)
+        self.strategies = strategies  # list of player's list of strategies
         self.payoffs = payoffs  # list of n payoff numpy arrays (A_1 x A_2 X ...)
         self.pnes = None  # list of pure nash equilibria (index of strategy for each player)
         self.mnes = None  # list of mixed nash equilibria (index of strategy for each player)
@@ -71,23 +66,32 @@ class Game:
         """ set payoff matrices """
         pass
 
+    def set_mnes(self):
+        """ set mixed nash equilibria """
+        pass
+
     def set_pnes(self):
         """ set pure nash equilibria """
         self.set_dependency(['payoffs'])
         cpnes = list(np.argwhere(self.payoffs[0] == np.amax(self.payoffs[0], 0)))
         cpnes = [tuple(cpne) for cpne in cpnes]
-        num_strat = [len(st) for st in self.strategies.values()]
-
+        pnes = []
         for i in range(1, self.n):
-            for cpne in cpnes:
-                ind = cpne[:i] + (slice(num_strat[i]),) + cpne[i+1:]
-                if self.payoffs[i][cpne] != np.amax(self.payoffs[i][ind]):
+            pm = self.payoffs[i]
+            for cpne in cpnes[:]:
+                ind = cpne[:i] + (slice(None),) + cpne[i+1:]
+                if pm[cpne] < np.max(pm[ind]):
                     cpnes.pop(cpnes.index(cpne))
         self.pnes = cpnes
 
-    def set_mnes(self):
-        """ set mixed nash equilibria """
-        pass
+    def set_st_dict(self):
+        """ set the dictionary of player labels and strategies """
+        st_product = product(*self.strategies)
+        labels = [j for j in product(*[[i for i in range(len(item))] for item in self.strategies])]
+        st_dict = {}
+        for k, v in zip(labels, st_product):
+            st_dict.update({k: v})
+        self.st_dict = st_dict
 
     def set_dependency(self, dependencies):
         """ set instance variables that are needed"""
@@ -95,26 +99,21 @@ class Game:
             if eval('self.' + dependency) is None:
                 eval('self.set_' + dependency + '()')
 
-    def set_st_dict(self):
-        """ set the dictionary of player labels and strategies """
-        st = [self.strategies[pl] for pl in self.players]
-        st_product = product(*st)
-        labels = [j for j in product(*[[i for i in range(len(item))] for item in st])]
-        st_dict = {}
-        for k, v in zip(labels, st_product):
-            st_dict.update({k: v})
-        self.st_dict = st_dict
-
     def check_game(self):
         """ function for enforcing correct construction of a game definition """
-        pass
+        if self.n != len(self.strategies):
+            raise ValueError('mismatch in length of self.strategies and number of players')
+        if self.payoffs and len(self.payoffs) != self.n:
+            raise ValueError('mismatch in length of self.payoffs and number of players')
+        if self.payoffs and list(np.shape(self.payoffs[0])) != [len(s) for s in self.strategies]:
+            raise ValueError('payoff matrix not set to right dimensions according to strategy set')
 
 
 class FromPayoffGame(Game):
     """ define players and strategies through the given payoff matrix"""
     def __init__(self, payoffs):
         players = [i for i in range(len(payoffs))]  # player labels: 1 ... n
-        strategies = {pl: [i for i in range(st)] for (pl, st) in zip(players, np.shape(payoffs[0]))}  # strategy labels: 1 ... m_i
+        strategies = [[i for i in range(d)] for d in np.shape(payoffs[0])]  # strategy labels: 1 ... m_i
         Game.__init__(self, payoffs, players, strategies)
 
 
@@ -123,5 +122,5 @@ class FromIndexGame(Game):
     no initialization of the payoff matrix"""
     def __init__(self, n, n_strat):
         players = [i for i in range(n)]  # player labels: 1 ... n
-        strategies = {pl: [i for i in range(st)] for (pl, st) in zip(players, n_strat)}  # strategy labels: 1 ... m_i
+        strategies = [[i for i in range(num)] for num in n_strat]  # strategy labels: 1 ... m_i
         Game.__init__(self, None, players, strategies)

Games/res/computable.py
@@ -4,9 +4,11 @@ Library for welfare resource games
 R Chandan, D Paccagnan, JR Marden (2019) Optimal mechanisms for distributed resource-allocation.
 """
 
-from Games.basic import *
+import numpy as np
 from Games.res.resource import DistResGame
 from cvxopt.solvers import lp
+from cvxopt import matrix
+from itertools import combinations
 #from scipy.optimize import linprog
 
 
@@ -14,7 +16,7 @@ def I(n):
     """ generate all viable triples of (a, x, b) with 1 <= a + x + b <= n"""
     ind = []
     for i in range(1, n+1):
-        all_i = [[j[0], j[1]-j[0]-1, i-j[1]+1] for j in combinations(range(i+2), 2)]
+        all_i = [(j[0], j[1]-j[0]-1, i-j[1]+1) for j in combinations(range(i+2), 2)]
         ind += all_i
     return ind
 
@@ -25,8 +27,8 @@ def I_r(n):
     ind = []
     for i in range(0, n+1):
         not_a = [(0, j, i) for j in range(n+1-i)]
-        not_b = [(j, 0, i) for j in range(n+1-i)]
-        not_x = [(j, i, 0) for j in range(n+1-i)]
+        not_x = [(j, 0, i) for j in range(n+1-i)]
+        not_b = [(j, i, 0) for j in range(n+1-i)]
         ind = ind + not_a + not_b + not_x
 
     ind += [(j[0], j[1]-j[0]-1, n-j[1]+1) for j in combinations(range(n+2), 2)]
@@ -39,87 +41,76 @@ class CompResourceGame(DistResGame):
         DistResGame.__init__(self, players, strategies, values, w, f)
         self.I = I(self.n)
         self.I_r = I_r(self.n)
+        self.solver = solver
 
-    def PoAfunction(self):
-        """ returns optimal function for the best Price of Anarchy """
-    
+    def function_poa(self):
+        """ returns the distribution rule f for the optimal PoA """
         num = len(self.I_r)
-        G = np.zeros((num+1, self.n+1))
-        h = np.zeros((num+1,))
-        h[num] = 1
+        G = np.zeros((num+1, self.n+1), dtype='float')
+        h = np.zeros((num+1, 1), dtype='float')
+        h[num] = -1
         
-        c = np.zeros((self.n+1,))
+        c = np.zeros((self.n+1, 1), dtype='float')
         c[0] = 1
+        for i in range(num):
+            a, x, b = self.I_r[i]
+            G[i, a+x] = a
+            if a+x < self.n:
+                G[i, a+x+1] = -b
+            G[i, 0] = -self.w[a+x]
+            h[i] = -self.w[b+x]
+        G[num][0] = -1
+
+        sol = self.poa_solver(c, G, h, None, None)
+        poa = 1/sol['primal objective']
+        f = [0.] + list(sol['x'])[1:]
+        return (poa, f)
+
+    def dual_poa(self):
+        """ dual formulation for calculation of Price of Anarchy"""  
+        if self.f[1] <= 0:
+            return 0
+        num = len(self.I_r)
+
+        G = np.zeros((num+1, 2), dtype='float')
+        h = np.zeros((num+1, 1), dtype='float')
+        c = np.array([[0], [1]], dtype='float')  # variables = [lambda , mu]
 
         for i in range(num):
-            a, b, x = self.I_r[i]
+            a, x, b = self.I_r[i]
+            G[i, 0] = a*self.f[a+x] - b*self.f[a+x+1] if a+x < self.n else a*self.f[a+x]
+            G[i, 1] = -self.w[a+x]
+            h[i] = -self.w[b+x]
+        G[num][0] = -1 
 
-            G[i, a+x] = a
-            G[i, a+x+1] = -b if a+x+1 <= self.n else 0
-            G[i, 1] = -self.W_r[0, a+x]
-            h[i] = -self.W_r[0, b+x]
-        
-        G[num][2] = 1
-        
-        solution = self.solver(c, G, h, None, None)
-        return solution
+        sol = self.poa_solver(c, G, h, None, None)
+        return 1/sol['primal objective']
 
-    def primalPoA(self):     
+    def primal_poa(self):     
         """ primal formulation for calculation of Price of Anarchy """
-
-        if self.f_r(1) <= 0:
+        if self.f[1] <= 0:
             return 0
-
         num = len(self.I)
 
-        c = [self.W_r[0, b+x] for a, b, x in self.I]
-
-        cons_1 = [a*self.f_r(a+x) - b*self.f_r(a+x+1) if a+x <= self. n else a*self.f_r(a+x)
-            for a, b, x in self.I]
-
+        c = -np.array([self.w[b+x] for a, x, b in self.I], dtype='float')
+        cons_1 = [a*self.f[a+x] - b*self.f[a+x+1] if a+x < self.n else a*self.f[a+x]
+                 for a, x, b in self.I]
         cons_2 = np.identity(num)
-        
-        G = np.vstack((cons_1, cons_2))
-        A = [self.W_r[0, a+x] for a, x, b in self.I]
-        b = np.ones((num,))
-        h = np.zeros(num+1,)
-        
-        return (c, G, h, A, b)
+        G = -np.vstack((cons_1, cons_2))
+        A = np.array([[self.w[a+x] for a, x, b in self.I]], dtype='float')
+        b = np.array([[1]], dtype='float')
+        h = np.zeros((num+1, 1))
 
-    def dualPoA(self):
-        """ dual formulation for calculation of Price of Anarchy"""  
+        sol = self.poa_solver(c, G, h, A, b)
+        return -1/sol['primal objective']
 
-        if self.f_r(1) <= 0:
-            return 0
-
-        num = len(self.I_r)
-        G = np.zeros((num+1, 2))
-        h = np.zeros((num+1,))
-        
-        c = np.array([0., 1])
-
-        for i in range(num):
-            a, b, x = self.I_r[i]
-
-            G[i, 0] = a*self.f_r(a+x) - b*self.f_r(a+x+1) if a+x+1 <= self.n else a*self.f_r(a+x)
-            G[i, 1] = -self.W_r[0, a+x]
-            h[i] = -self.W_r[0, b+x]
-        
-        G[num][0] = 1 
-        
-        solution = self.solver(c, G, h, None, None)
-        return solution
-
-    def f_r(self, num):
-        a = [0, 1, 2, 3]
-        try:
-            return a[num]
-        except Exception as e:
-            print(num)
-            raise e
-
-    def solver(self, c, G, h, A, b):
+    def poa_solver(self, c, G, h, A, b):
+        """ function for solving the relevant optimization program"""
         if self.solver == 'cvxopt':
-            return lp(c, G, h, A, b)
+            sol = lp(matrix(c), matrix(G), matrix(h), matrix(A) if A else None, matrix(b) if b else None)
+            if sol['status'] == 'optimal':
+                return sol
+            else:
+                raise ValueError('no feasible solution found')
         else:
             raise ValueError('indicated a invalid solver name for self.solver')

Games/res/resource.py
@@ -74,7 +74,7 @@ class CongestionGame(ResourceGame):
         return self.Fr[res, len(players)]
 
 
-class DistrResGame(CongestionGame):
+class DistResGame(CongestionGame):
     """ distributed resource allocation game where utility is gained for covering an array of resources """
     def __init__(self, players, strategies, values, w, f):
         Wr = np.dot(np.diag(values), np.array([w] * len(values)))  # Wr(j) = v_r * w(j)

Games/res/testing.py
@@ -3,26 +3,25 @@
 Tutorial example for computing price of anarchy for a class of resource allocation games
 """
 
-#from computable import *
+from Games.basic import *
 from resource import *
 
-
-players = ['Alice', 'Bob', 'Carolyn', 'Dmitri']
-res = [4, 9, 2, 5, 8, 7, 9, 3, 6, 7]
-r_m = len(res)
-n = len(players)
-strategies = [[(), (1, 2, 4), (3, 6), (0, 9), (7, 8)], [(), (2, 8, 9), (3, 4, 5), (6, 7, 9), (0, 4)], [(), (5, 7), (0, 6), (3, 6, 9), (1, 2, 6)], [(), (2, 3), (4, 8), (1, 3)]]
-
-players = ['Alice', 'Bob']
-strategies = [[(3, 2), (1, 2), (3, 4)], [(), (3, 4, 5),  (0, 4)]]
-r_m = 6
-
-# Vehicle Target Assignment
-p = .5
-w = [1 - (1-p)**j for j in range(n+1)]
-f = [0] + [p*(1-p)**(j-1) for j in range(1, n+1)]
-
-game = DistrResGame(players, to_dictstrat(players, strategies))
-#game.set_s_payoff()
+players = range(1, 5)
+strategies = [[(), (0,), (2,)], [(), (1,), (2,)], [(), (2,), (3,), (4,)], [(), (2,), (3,), (4,)]]
+res = [2, 1, 4, 2, 1]
+w = [0, 1, 1, 1, 1]
+f = [0, 1, 0, 0, 0]
 
 
+
+game = DistResGame(players, strategies, res, w, f)
+
+game.set_so()
+game.set_posa()
+#for i in np.ndenumerate(game.payoffs[0]):
+#	print(i)
+game.set_pnes()
+print(game.pnes)
+print(np.array([game.s_payoff[pne] for pne in game.pnes]))
+print(game.so)
+print(game.posa)

Games/social.py
@@ -11,7 +11,7 @@ class SocialGame(Game):
     def __init__(self, payoffs, players, strategies):
         Game.__init__(self, payoffs, players, strategies)
         self.s_payoff = None  # social payoff for each set of strategies
-        self.so = None  # optimal social payoff
+        self.so = None  # one instance of a strategy for the optimal social payoff
         self.posa = None  # (Price of Anarchy, Stability)
 
     def set_s_payoff(self):
@@ -21,13 +21,13 @@ class SocialGame(Game):
     def set_so(self):
         """ get strategies for the optimal social payoff"""
         self.set_dependency(['s_payoff'])
-        self.so = np.amax(self.s_payoff)
+        self.so = np.unravel_index(np.argmax(self.s_payoff), self.s_payoff.shape)
 
     def set_posa(self):
         """ set price of anarchy and stability """
         self.set_dependency(['pnes', 'so'])
-        pne_values = np.array([self.s_payoff[pne] for pne in self.pnes])
-        price_ratios = list(pne_values/self.so)
+        pne_values = np.array([self.s_payoff[pne] for pne in self.pnes], dtype='float')
+        price_ratios = list(pne_values/self.s_payoff[self.so])
         self.posa = (min(price_ratios), max(price_ratios))  # poa, pos
 
 
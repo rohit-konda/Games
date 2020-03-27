99cc5b3ac23194f05be870fc0e777d4eaa239f20
rohit-konda <rohit-konda@users.noreply.github.com>
2020-03-26 18:02:02 -0700

    Update player_test.py

--- games/tests/player_test.py
+++ games/tests/player_test.py
@@ -1,13 +1,34 @@
 from games.types.players import *
-import unittest
+import unittest as ut
 
 
-class TestPlayers(unittest.TestCase):
-	def test_constructor(self):
-		pass
+class TestPlayers(ut.TestCase):
+	def test_player(self):
+		name = 'test'
+		index = 0
+		actions = Actions()
+		def util(play, board):
+			return play[0]
+		player = Player(name, index, actions, util)
+		play, board = [0], None
+		self.assertEqual(player.U(play, board), 0)
+		self.assertIsNone(player.move(play, board))
 
-class TestActions(unittest.TestCase):
-	pass
 
-class TestFActions(unittest.TestCase):
-	pass
+class TestFActions(ut.TestCase):
+	def test_FActions(self):
+		name = 'p1'
+		actions = [1, 2]
+		fact = FActions(name, actions)
+		self.assertEqual(fact(0, None), 1)
+		self.assertEqual(fact[0], 1)
+		self.assertEqual([i for i in fact], actions)
+		self.assertEqual(fact.name, 'p1')
+		self.assertEqual(len(fact), 2)
+
+		def testequaler(name): fact.name = name
+		self.assertWarns(Warning, testequaler, 'p2')
+		self.assertRaises(IndexError, fact, 2, None)
+
+if __name__ == '__main__':
+	ut.main()
